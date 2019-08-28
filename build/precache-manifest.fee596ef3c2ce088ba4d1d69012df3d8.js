4bb9defbf9af46d66ff91890ea24d1356f9c2ce8
Syamsul <syamsul@alterra.id>
2019-08-28 15:15:59 +0700

    automate deploy

---
+++ build/precache-manifest.fee596ef3c2ce088ba4d1d69012df3d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdb7af3f275378fb2fb8b58d1cca376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64e91b39252885b2ced0",
+    "url": "/static/css/main.5a7c569d.chunk.css"
+  },
+  {
+    "revision": "74f10c0e960e53d5a557",
+    "url": "/static/js/2.b7f5cf67.chunk.js"
+  },
+  {
+    "revision": "64e91b39252885b2ced0",
+    "url": "/static/js/main.9d924e05.chunk.js"
+  },
+  {
+    "revision": "abe75fb1d87b5abf77e3",
+    "url": "/static/js/runtime~main.93ba48e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
