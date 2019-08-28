55526c9f03645d64499fc5e1597c60f08fe183d7
Syamsul <syamsul@alterra.id>
2019-08-28 19:37:22 +0700

    tes

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ sudo docker stop syamsuldocker/frontend_automate
 sudo docker rm syamsuldocker/frontend_automate
 sudo docker rmi syamsuldocker/frontend_automate
 sudo docker pull syamsuldocker/frontend_automate
-# sudo docker stop 6d3d7fb58356
+sudo docker stop 6d3d7fb58356
 # sudo docker run -d -p 3000:80 syamsuldocker/frontend_automate:latest
