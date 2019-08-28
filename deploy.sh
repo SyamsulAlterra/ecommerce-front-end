4f86f501bb52e90a64c504666af95e382131908d
Syamsul <syamsul@alterra.id>
2019-08-28 21:35:23 +0700

    tes

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,9 @@
 # source ~/.profile
 # echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
 
-sudo docker stop syamsuldocker/frontend_automate
-sudo docker rm syamsuldocker/frontend_automate
+sudo docker stop fb19eecef3d3
+sudo docker rm fb19eecef3d3
+
 sudo docker rmi syamsuldocker/frontend_automate
 sudo docker pull syamsuldocker/frontend_automate
-# sudo docker stop 6d3d7fb58356
 sudo docker run -d -p 3000:80 syamsuldocker/frontend_automate:latest
